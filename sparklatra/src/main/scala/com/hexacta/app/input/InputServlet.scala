2b44f732c643b50c0b69202d9f7219a6bdb1855c
Constanza Quaglia <cquaglia@hexacta.com>
2018-10-31 17:16:17 -0300

    Se agrega metodo para guardar un usuario

--- sparklatra/src/main/scala/com/hexacta/app/input/InputServlet.scala
+++ sparklatra/src/main/scala/com/hexacta/app/input/InputServlet.scala
@@ -2,7 +2,11 @@ package com.hexacta.app.input
 
 import java.util
 
-import com.hexacta.app.model.{Repo, User}
+import com.hexacta.app.model.{Change, Repo, User}
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.hbase.client.{ConnectionFactory, Put}
+import org.apache.hadoop.hbase.util.Bytes
+import org.apache.hadoop.hbase.{HBaseConfiguration, TableName}
 import org.json4s.JsonAST.{JArray, JObject, JString}
 import org.scalatra._
 import org.json4s.JsonDSL._
@@ -14,6 +18,8 @@ class InputServlet extends ScalatraServlet {
 
   get("/") {
     try {
+
+
       val content = getRest("https://api.github.com/users?since=0")
       val json = parse(content)
       var users = ListBuffer[User]()
@@ -41,6 +47,43 @@ class InputServlet extends ScalatraServlet {
     }
   }
 
+  def saveUser(user: User): Unit = {
+    val conf : Configuration = HBaseConfiguration.create()
+    // Se debe agregar en el archivo host la siguiente entrada:
+    // 10.60.1.27   quickstart.cloudera
+    conf.set("hbase.zookeeper.quorum", "quickstart.cloudera")
+    conf.set("hbase.zookeeper.property.clientPort", "2181")
+
+    conf.set("hbase.master.port", "60000") //16000
+    conf.set("hbase.master.info.port", "60010")//16010
+    conf.set("hbase.regionserver.info.port", "60030")
+    conf.set("hbase.regionserver.port", "60020")
+    // cree la tabla
+    // create 'TableTest', 'info'
+    // put 'TableTest', 'rowkey1', 'info:test', 'ejemplo'
+    val connection = ConnectionFactory.createConnection(conf)
+    val table = connection.getTable(TableName.valueOf( "changes" ))
+
+    for {
+      repo <- user.repos;
+      commit <- repo.commits;
+      change <- commit.changes
+    } {
+      val key = "%s:%s:%s:%s".format(user.userName, repo.name, commit.sha, change.fileSha)
+      val put = new Put(Bytes.toBytes(params(key)))
+
+      put.addColumn(Bytes.toBytes("user"), Bytes.toBytes("name"), Bytes.toBytes(params(user.userName)))
+      put.addColumn(Bytes.toBytes("repo"), Bytes.toBytes("name"), Bytes.toBytes(params(repo.name)))
+      put.addColumn(Bytes.toBytes("commit"), Bytes.toBytes("sha"), Bytes.toBytes(params(commit.sha)))
+      put.addColumn(Bytes.toBytes("change"), Bytes.toBytes("filesha"), Bytes.toBytes(params(change.fileSha)))
+      put.addColumn(Bytes.toBytes("change"), Bytes.toBytes("delete"), Bytes.toBytes(params(change.delete.toString)))
+      put.addColumn(Bytes.toBytes("change"), Bytes.toBytes("addition"), Bytes.toBytes(params(change.addition.toString)))
+      put.addColumn(Bytes.toBytes("change"), Bytes.toBytes("change"), Bytes.toBytes(params(change.changes.toString)))
+      put.addColumn(Bytes.toBytes("change"), Bytes.toBytes("fileextension"), Bytes.toBytes(params(change.extension)))
+
+      table.put(put)
+    }
+  }
 
   @throws(classOf[java.io.IOException])
   @throws(classOf[java.net.SocketTimeoutException])
--- sparklatra/src/main/scala/com/hexacta/app/model/Change.scala
+++ sparklatra/src/main/scala/com/hexacta/app/model/Change.scala
@@ -1,5 +1,5 @@
 package com.hexacta.app.model
 
-class Change (val total:Int, val addition: Int, val delete: Int, val changes: Int, val extension: String){
+class Change (val total:Int, val addition: Int, val delete: Int, val changes: Int, val extension: String, val fileSha: String){
 
 }
