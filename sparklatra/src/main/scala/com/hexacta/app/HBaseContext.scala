fbeac8befb591a773f52e6e13b75c0e9c41fc0d8
pgoette <pgoette@hexacta.com>
2019-05-10 11:39:24 -0300

    ejemplo con hortonwork

--- sparklatra/src/main/scala/com/hexacta/app/HBaseContext.scala
+++ sparklatra/src/main/scala/com/hexacta/app/HBaseContext.scala
@@ -9,14 +9,16 @@ object HBaseContext {
   val conf : Configuration = HBaseConfiguration.create()
   // Se debe agregar en el archivo host la siguiente entrada:
   // 10.60.1.31   quickstart.cloudera
-  conf.set("hbase.zookeeper.quorum", "quickstart.cloudera")
+  // 10.30.15.192 sandbox-hdp.hortonworks.com
+  conf.set("hbase.zookeeper.quorum", "sandbox-hdp.hortonworks.com")
   conf.set("hbase.zookeeper.property.clientPort", "2181")
 
-  conf.set("hbase.master", "quickstart.cloudera:60000")
-  conf.set("hbase.master.port", "60000") //16000
-  conf.set("hbase.master.info.port", "60010")//16010
-  conf.set("hbase.regionserver.info.port", "60030")
-  conf.set("hbase.regionserver.port", "60020")
+  conf.set("hbase.master", "sandbox-hdp.hortonworks.com:16000")
+  conf.set("hbase.master.port", "16000") //16000
+  conf.set("hbase.master.info.port", "16010")//16010
+  conf.set("hbase.regionserver.info.port", "16030")
+  conf.set("hbase.regionserver.port", "16020")
+  conf.set("zookeeper.znode.parent", "/hbase-unsecure") //se configura en hortonworks porque corre en cluster
 
   def getConf() :Configuration = conf
 
